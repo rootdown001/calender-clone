cc8dd139c4e27741cd0add210e77aab48a10020c
Lance Anderson <lance.a.585@gmail.com>
2023-12-23 20:06:14 -0800

    ADD html - added starting html ADD css - add starting css CREATE Calendar - created calendar component ADD date-fns ADD loagic - added some starting logic to Calendar module for today, old date

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Calendar from "./Calendar";
+import CalendarCopy from "./CalendarCopy";
+
+export default function App() {
+  return (
+    <>
+      <Calendar />
+      {/* <CalendarCopy /> */}
+    </>
+  );
+}
